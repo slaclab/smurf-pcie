67338727bfff7427daba10867c5292e893eb81a4
Steve Smith <ssmith@slac.stanford.edu>
2015-11-19 23:38:24 +0000

    initial version of setupCryoFPGA.m Matlab script to setup parameters used by CryoDet.slx
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/LLRF/firmware@8436 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 76f7ad10e9638509ad4c6379453f3ff92fbbe9ea

---
+++ modules/DspCoreLib/CryoDet/simulink/setupCryoFPGA.m
@@ -0,0 +1,21 @@
+% script   setupCryoDet
+% set up parameters for CryoDet.slx
+format compact
+
+Fadc = 370e6  % ADC clock  frequency
+Ts = 1/Fadc  % ADC sample rate
+Fclk = Fadc/2  % data to FPGA is brought out in 2 parallel cahnnels at half the ADC rate
+Tclk = 1/Fclk
+
+% Cryo Mux transfer function simulatation parameters
+
+FIR1length = 30
+FIR1BW = 0.25
+
+%white noise generator paramters
+noiseLen = 128
+noiseBW = 2e6
+noiseSteps = 32
+noise = ifft(exp(2i*pi*rand(1, noiseLen)) .* fft(fir1(noiseLen-1, noiseBW/Fclk*noiseSteps)));
+
+
