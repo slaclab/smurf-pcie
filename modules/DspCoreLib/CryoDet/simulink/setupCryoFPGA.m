0de943b89614369d5fea82ce3008b6b9a5f6b41f
Steve Smith <ssmith@slac.stanford.edu>
2016-01-01 02:56:32 +0000

    Replaced multiplicative white noise modulator with phase shift modulator replaced random phase noise generator with chirp frequency modulator
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/LLRF/firmware@8639 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: f6dff16b581f8f38e45c615d4569508ec460d790

--- modules/DspCoreLib/CryoDet/simulink/setupCryoFPGA.m
+++ modules/DspCoreLib/CryoDet/simulink/setupCryoFPGA.m
@@ -1,7 +1,7 @@
 % script   setupCryoDet
 % set up parameters for CryoDet.slx
 format compact
-setupVers = 3; % just a number that goes into a status register
+setupVers = 4; % just a number that goes into a status register
 
 Fadc = 370e6  % ADC clock  frequency
 Ts = 1/Fadc  % ADC sample rate
@@ -25,7 +25,8 @@ Tclk = 1/Fclk
 %  185/32/3 = 1.93 MHz
 %  185/64/2 = 1.445 MHz
 %  185/64/3 = 0.964 MHz
-FIR2 = conv([1 2 3 4 5 6 7 8 7 6 5 4 3 2 1]/64,conv(ones(1,16)/16, ones(1,24)/24)); 
+%FIR2 = conv([1 2 3 4 5 6 7 8 7 6 5 4 3 2 1]/64, conv(ones(1,16)/16, ones(1,24)/24)); %notches @ 2.89 MHz, 1.445 MHz, 0.964 MHz
+FIR2 = conv([1 2 3 4 5 6 7 8 7 6 5 4 3 2 1]/64, ones(1,16)/16); %notches @ 2.89 MHz & 1.445 MHz
 figure(20),plot(FIR2),grid
 y =FIR2; Nx=4; y(185*Nx) = 0; Nfir = length(y); nfir=1:Nfir; ffir = (nfir-1)/8/Nx;
 figure(21), plot(ffir, 20*log10(abs(fft(y)))),grid
@@ -34,7 +35,29 @@ figure(21), plot(ffir, 20*log10(abs(fft(y)))),grid
 noiseLen = 128
 noiseBW = 2e6
 noiseSteps = 32
-noise = ifft(exp(2i*pi*rand(1, noiseLen)) .* fft(fir1(noiseLen-1, noiseBW/Fclk*noiseSteps)));
+%noise = ifft(exp(2i*pi*rand(1, noiseLen)) .* fft(fir1(noiseLen-1, noiseBW/Fclk*noiseSteps)));
+%rmsnoise = sqrt(mean(abs(noise.*noise)));
+%noise = noise/1.4/rmsnoise;
+%for n=1:noiseLen
+%    if abs(noise(n)) > 1
+%        noise(n) = noise(n)/abs(noise(n)); %Clip noise peaks
+%    end
+%end
+dt = noiseSteps/Fclk; % time increment for noise phase modulation
+
+for n=1:noiseLen
+    if n <= (noiseLen/2)
+        f(n) = noiseBW*(4*n/noiseLen - 1); %rising chirp
+    else
+        f(n) = f(noiseLen + 1 - n); % descending chirp
+    end
+    if n==1
+        phi(n) = 2*pi*f(1)*dt;
+    else
+        phi(n) = phi(n-1) + 2*pi*f(n)*dt; %integrate freq to get phase
+    end
+end
+noise = exp(1i*phi);
 
 % Simulate resonator notch in simulink
 Fnotch = 70e6, BW = 1e6, Q = Fnotch/BW, wNotch = 2*pi*Fnotch
