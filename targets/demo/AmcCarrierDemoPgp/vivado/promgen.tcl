0376ffd1c4413236e955d9ee15e32b46ca0719f8
Larry Ruckman <ruckman@slac.stanford.edu>
2016-08-05 22:54:37 +0000

    1) Updated all the firmware Makefiles to use the released tags instead of the SVN HEAD 2) Slightly changed the file structures for tags and head
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/LLRF/firmware@11727 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 3db49e32e41476318af630cc9722ba0a16248514

--- targets/demo/AmcCarrierDemoPgp/vivado/promgen.tcl
+++ targets/demo/AmcCarrierDemoPgp/vivado/promgen.tcl
@@ -1,15 +1,15 @@
 ##############################################################################
-## This file is part of 'LCLS2 LLRF Development'.
+## This file is part of 'LCLS2 AMC Carrier Firmware'.
 ## It is subject to the license terms in the LICENSE.txt file found in the 
 ## top-level directory of this distribution and at: 
 ##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
-## No part of 'LCLS2 LLRF Development', including this file, 
+## No part of 'LCLS2 AMC Carrier Firmware', including this file, 
 ## may be copied, modified, propagated, or distributed except according to 
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
 
 ## Source the AMC Carrier Core's .TCL file
-source $::env(PROJ_DIR)/../../../modules/AmcCarrierCore/vivado/promgen.tcl
+source $::env(PROJ_DIR)/../../../modules/AmcCarrierCore/$::env(AMC_TAG)/vivado/promgen.tcl
 
 ## Setup the .BIT file and user file configurations
 set loadbit    "up ${LCLS_II_BIT} ${BIT_PATH}"
