a23cb553bb4caa77fa3a1dd9b79f16dd0e077635
Uros Legat <ulegat@slac.stanford.edu>
2016-09-16 21:00:26 +0000

    Upgrading MR target to new yaml schema
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/LLRF/firmware@12506 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 83e08b30cd742bc7ad5b9cff43b3933988671fb8
    Former-commit-id: 76fff6c8e5d1113340deabb3632f5fe627ebefe2

--- targets/demo/AmcCarrierDemoEth/Version.vhd
+++ targets/demo/AmcCarrierDemoEth/Version.vhd
@@ -13,9 +13,9 @@ use ieee.std_logic_1164.all;
 
 package Version is
 
-   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000004";  -- MAKE_VERSION
+   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000005";  -- MAKE_VERSION
 
-   constant BUILD_STAMP_C : string := "AmcCarrierDemoEth: Vivado v2016.2 (x86_64) Built Thu Sep 15 15:49:25 PDT 2016 by ulegat";
+   constant BUILD_STAMP_C : string := "AmcCarrierDemoEth: Vivado v2016.2 (x86_64) Built Fri Sep 16 09:22:13 PDT 2016 by ulegat";
 
 end Version;
 
@@ -24,7 +24,8 @@ end Version;
 --
 -- 09/09/2016 (0x00000001): Initial Build without Sysgen
 -- 09/12/2016 (0x00000002): With Sysgen
--- 09/12/2016 (0x00000003): New yaml
---
+-- 09/14/2016 (0x00000003): New yaml
+-- 09/15/2016 (0x00000004): Updated diffswing xci - Works
+-- 09/15/2016 (0x00000005): Updated diffswing dcp - Does not work
 -------------------------------------------------------------------------------
 
