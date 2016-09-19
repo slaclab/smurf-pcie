83be50f4a77f2787b067e987292b36c8f7d59ac3
Uros Legat <ulegat@slac.stanford.edu>
2016-09-19 23:36:36 +0000

    only bay 1
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk/LLRF/firmware@12517 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: e11d83bd233d7996e2453d661d3f0ad23ca6214a
    Former-commit-id: f6b28f55c87cb2cc4903e1cfa1e220b35350f3d6

--- targets/demo/AmcCarrierDemoEth/Version.vhd
+++ targets/demo/AmcCarrierDemoEth/Version.vhd
@@ -13,7 +13,7 @@ use ieee.std_logic_1164.all;
 
 package Version is
 
-   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000005";  -- MAKE_VERSION
+   constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"00000004";  -- MAKE_VERSION
 
    constant BUILD_STAMP_C : string := "AmcCarrierDemoEth: Vivado v2016.2 (x86_64) Built Fri Sep 16 09:22:13 PDT 2016 by ulegat";
 
