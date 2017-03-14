26658048b78f126bc5a25385fa529f6431bcaadd
Uros Legat <ulegat@slac.stanford.edu>
2017-03-14 13:49:45 -0700

    Adding the kcu60 and kcu test placeholder

---
+++ firmware/common/EvalBoardTest/ruckus.tcl
@@ -0,0 +1,10 @@
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load local Source Code and constraints
+loadSource      -dir  "$::DIR_PATH/hdl/"
+#loadConstraints -dir  "$::DIR_PATH/xdc/"
+#loadSource -sim_only -dir "$::DIR_PATH/tb/"
+
+#loadSource      -path "$::DIR_PATH/coregen/AppTopJesd204bCoregen.dcp"
+loadSource    -path "$::DIR_PATH/coregen/KcuJesd204bCoregen.xci"
