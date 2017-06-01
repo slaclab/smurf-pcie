e4f7136d2bff7d6db67e9d4f3d87e3d9eb9efea0
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-05-31 23:50:37 -0700

    Add comments to the test scripts

--- software/issues/rogue/cpsw/test.py
+++ software/issues/rogue/cpsw/test.py
@@ -4,15 +4,11 @@ import pycpsw
 root       = pycpsw.Path.loadYamlFile("yaml/000TopLevel.yaml")
 counter    = pycpsw.ScalVal_RO.create(root.findByName("mmio/AmcCarrierCore/AxiVersion/UpTimeCnt"))
 
-dacID      = pycpsw.ScalVal_RO.create(root.findByName("mmio/AppTop/AppCore/AmcCryoCore[1]/Dac38J84[0]/ID"))
-dacTemp    = pycpsw.ScalVal_RO.create(root.findByName("mmio/AppTop/AppCore/AmcCryoCore[1]/Dac38J84[0]/Temperature"))
-
-#print("DAC_0 ID: 0x%x" % (cryo.AppTop.AppCore.AmcCryoCore_1.Dac38J84_0.ID.get()))
-#print("DAC_1 ID: 0x%x" % (cryo.AppTop.AppCore.AmcCryoCore_1.Dac38J84_1.ID.get()))
-#print("DAC_0 Temperature description: %s" % (cryo.AppTop.AppCore.AmcCryoCore_1.Dac38J84_0.Temperature.description))
-#print("DAC_0 Temperature: %s" % (cryo.AppTop.AppCore.AmcCryoCore_1.Dac38J84_0.Temperature.get()))
-
+dac0ID     = pycpsw.ScalVal_RO.create(root.findByName("mmio/AppTop/AppCore/AmcCryoCore[1]/Dac38J84[0]/ID"))
+dac1ID     = pycpsw.ScalVal_RO.create(root.findByName("mmio/AppTop/AppCore/AmcCryoCore[1]/Dac38J84[1]/ID"))
+dac0Temp   = pycpsw.ScalVal_RO.create(root.findByName("mmio/AppTop/AppCore/AmcCryoCore[1]/Dac38J84[0]/Temperature"))
 
 print "UpTime Counter:     %d"   % counter.getVal()
-print "Dac[0] ID:          0x%x" % dacID.getVal()
-print "Dac[0] Temperature: %d"   % dacTemp.getVal()
+print "Dac[0] ID:          0x%x" % dac0ID.getVal()
+print "Dac[1] ID:          0x%x" % dac1ID.getVal()
+print "Dac[0] Temperature: %d"   % dac0Temp.getVal()
