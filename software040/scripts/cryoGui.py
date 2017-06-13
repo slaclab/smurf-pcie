c8407bb8e5acc18a9eae451659fbc705b244ca6d
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-06-13 16:20:38 -0700

    Updating cryoGui

--- software040/scripts/cryoGui.py
+++ software040/scripts/cryoGui.py
@@ -16,7 +16,7 @@ pyrogue.streamConnectBiDir(rssiSrp,udpRssiA.application(0))
 
 cryo = pyrogue.Root('cryo','AMC Carrier')
 
-#cryo.add(AmcCarrierCore(memBase=rssiSrp, offset=0x00000000))
+cryo.add(AmcCarrierCore(memBase=rssiSrp, offset=0x00000000))
 cryo.add(AppTop(memBase=rssiSrp, offset=0x80000000, numRxLanes=[0,8], numTxLanes=[0,8]))
 
 #cryo.setTimeout(5.0)
