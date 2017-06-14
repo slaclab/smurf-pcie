76ee32b3aefc6ed46c459919a5bb499ef0042686
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-06-13 17:50:32 -0700

    Update cryoGui

--- software040/scripts/cryoGui.py
+++ software040/scripts/cryoGui.py
@@ -3,6 +3,8 @@ import pyrogue
 import pyrogue.protocols
 import rogue.protocols.srp
 import rogue.protocols.udp
+import pyrogue.utilities.fileio
+import rogue.interfaces.stream
 import PyQt4.QtGui
 import pyrogue.gui
 import sys
@@ -10,27 +12,81 @@ import sys
 from AmcCarrierCore import *
 from AppTop import *
 
-udpRssiA = pyrogue.protocols.UdpRssiPack("10.0.3.106",8193,1500)
-rssiSrp= rogue.protocols.srp.SrpV3()
-pyrogue.streamConnectBiDir(rssiSrp,udpRssiA.application(0))
+# Custom run control
+class MyRunControl(pyrogue.RunControl):
+   def __init__(self,name):
+      pyrogue.RunControl.__init__(self,name=name,description='Run Controller',
+                                  rates={1:'1 Hz', 10:'10 Hz', 30:'30 Hz'})
+      self._thread = None
 
-cryo = pyrogue.Root('cryo','AMC Carrier')
+   def _setRunState(self,dev,var,value,changed):
+      if changed:
+         if self.runState.get(read=False) == 'Running':
+            self._thread = threading.Thread(target=self._run)
+            self._thread.start()
+         else:
+            self._thread.join()
+            self._thread = None
 
-cryo.add(AmcCarrierCore(memBase=rssiSrp, offset=0x00000000))
-cryo.add(AppTop(memBase=rssiSrp, offset=0x80000000, numRxLanes=[0,8], numTxLanes=[0,8]))
+   def _run(self):
+      self.runCount.set(0)
+      self._last = int(time.time())
 
-#cryo.setTimeout(5.0)
+      while (self.runState.get(read=False) == 'Running'):
+         delay = 1.0 / ({value: key for key,value in self.runRate.enum.items()}[self._runRate])
+         time.sleep(delay)
+         self._root.ssiPrbsTx.oneShot()
 
-# Create GUI
-appTop = PyQt4.QtGui.QApplication(sys.argv)
-guiTop = pyrogue.gui.GuiTop(group='cryoMesh')
-guiTop.resize(800, 1000)
-guiTop.addTree(cryo)
+         self._runCount += 1
+         if self._last != int(time.time()):
+             self._last = int(time.time())
+             self.runCount._updated()
+  
 
-print("Starting GUI...\n");
+class cryoGui(pyrogue.Root):
 
-# Run GUI
-appTop.exec_()
+   def __init__(self):
 
-cryo.stop()
-exit()
+      pyrogue.Root.__init__(self,'cryo','AMC Carrier')
+      
+      # Run control
+      self.add(MyRunControl('runControl'))
+
+      # File writer
+      dataWriter = pyrogue.utilities.fileio.StreamWriter('dataWriter')
+      self.add(dataWriter)
+
+      # Create RSSI interface
+      udpRssiA = pyrogue.protocols.UdpRssiPack("10.0.3.106",8193,1500)
+      
+      # Create data stream interface
+      
+      # Create and connect SRP to RSSI
+      rssiSrp= rogue.protocols.srp.SrpV3()
+      pyrogue.streamConnectBiDir(rssiSrp,udpRssiA.application(0))
+      
+      # Add data stream to file as channel 0
+
+      # Add devices     
+      self.add(AmcCarrierCore(memBase=rssiSrp, offset=0x00000000))
+      #cryo.add(AppTop(memBase=rssiSrp, offset=0x80000000, numRxLanes=[0,8], numTxLanes=[0,8]))
+      
+      # Set global timeout
+      self.setTimeout(1.0)
+      
+      # Create GUI
+      appTop = PyQt4.QtGui.QApplication(sys.argv)
+      guiTop = pyrogue.gui.GuiTop(group='cryoMesh')
+      guiTop.resize(800, 1000)
+      guiTop.addTree(self)
+      
+      print("Starting GUI...\n");
+      
+      # Run GUI
+      appTop.exec_()
+   
+      super().stop()
+      exit()
+
+if __name__ == "__main__":
+   cryoGui = cryoGui()
