f50c5a040dc99037402cffc640cb6a0ff720876e
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-06-22 15:00:07 -0700

    Add FpgaTopLevel pyrogue class to AmcCarrierDemoRtmEth target

---
+++ firmware/targets/AmcCarrierDemoRtmEth/python/FpgaTopLevel/FpgaTopLevel.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Title      : PyRogue AMC Carrier Cryo Demo Board Application
+#-----------------------------------------------------------------------------
+# File       : AppCore.py
+# Created    : 2017-04-03
+#-----------------------------------------------------------------------------
+# Description:
+# PyRogue AMC Carrier Cryo Demo Board Application
+#-----------------------------------------------------------------------------
+# This file is part of the rogue software platform. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the rogue software platform, including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+import pyrogue as pr
+
+from AmcCarrierCore import *
+from AppTop import *
+
+class FpgaTopLevel(pr.Device):
+    def __init__(   self, 
+                    name        = "FpgaTopLevel", 
+                    description = "FPGA Top-Level", 
+                    memBase     =  None, 
+                    offset      =  0x0, 
+                    hidden      =  False,
+                    expand      =  True,
+                ):
+        super(self.__class__, self).__init__(name, description, memBase, offset, hidden, expand=expand)
+        
+        self.add(AmcCarrierCore(offset=0x00000000, enableBsa=False))
+        # self.add(AppTop(        offset=0x80000000, numRxLanes=[6,0], numTxLanes=[2,0])) #BAY[0] only
+        self.add(AppTop(        offset=0x80000000, numRxLanes=[0,6], numTxLanes=[0,2])) #BAY[1] only
+    
