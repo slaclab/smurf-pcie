941d3cfb8eabb6dba6b33bd0148bbde85bb2a878
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-06-22 14:56:57 -0700

    Update structure of DemoBoardCryoApp folder

--- firmware/common/DemoBoardCryoApp/ruckus.tcl
+++ firmware/common/DemoBoardCryoApp/ruckus.tcl
@@ -2,7 +2,4 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
-loadSource -dir "$::DIR_PATH/hdl/"
-
-# Load submodules' code and constraints
-loadRuckusTcl "$::DIR_PATH/../DspCoreLib/CryoDetEth"
+loadRuckusTcl "$::DIR_PATH/AppCore"
