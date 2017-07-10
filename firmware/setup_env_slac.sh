c8b6bd7992fa3db92ba7b349f123cbb08a1afe57
Larry Ruckman <ruckman@slac.stanford.edu>
2017-07-10 13:35:53 -0700

    updating scripts for sysgen

--- firmware/setup_env_slac.sh
+++ firmware/setup_env_slac.sh
@@ -10,3 +10,6 @@
 
 # Setup the Xilinx software and licensing
 source /afs/slac/g/reseng/xilinx/vivado_2016.4/Vivado/2016.4/settings64.sh
+
+# Setup System Generator Environment
+source /afs/slac/g/reseng/xilinx/vivado_2016.4/Vivado/2016.4/.settings64-sysgen.sh
