f892c1a31670951e926004e6fbfbc5c3f4cf3970
Larry Ruckman <ruckman@slac.stanford.edu>
2017-09-12 15:11:13 -0700

    copying cryo-det and creating a XRAY and CMB version

---
+++ firmware/common/DspCoreLib/CryoDetXray/simulink/setupADCtoDACloopback.m
@@ -0,0 +1,9 @@
+%setupADCtoDACloopback
+
+% script to define variables used by ADCtoDACloopback.slx
+% SS 17Nov2015
+format compact
+ %Fs = 307.2e6;  
+ Fs = 185e6;
+ Ts = 1/Fs; 
+ Fgen = 20e6
---
+++ firmware/targets/CryoXrayRtmEth/python/FpgaTopLevel/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+from FpgaTopLevel.FpgaTopLevel import *
