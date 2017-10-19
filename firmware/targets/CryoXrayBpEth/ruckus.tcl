47c273297df4bc8b982bac8c3dae22814dc75915
Larry Ruckman <ruckman@slac.stanford.edu>
2017-10-19 13:31:26 -0700

    updating the Place and Route strategy

--- firmware/targets/CryoXrayBpEth/ruckus.tcl
+++ firmware/targets/CryoXrayBpEth/ruckus.tcl
@@ -11,3 +11,6 @@ loadRuckusTcl $::env(TOP_DIR)/submodules
 # Load target's source code and constraints
 loadSource      -dir  "$::DIR_PATH/hdl/"
 loadConstraints -dir  "$::DIR_PATH/hdl/"
+
+# Place and Route strategy 
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
