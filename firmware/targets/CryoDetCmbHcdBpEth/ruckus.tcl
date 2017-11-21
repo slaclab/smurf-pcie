411210c668e2f0ddaa8ce0d6f1d85927577cca84
Mitch D'Ewart <mdewart@slac.stanford.edu>
2017-11-21 10:28:38 -0800

    Add power optimization options

--- firmware/targets/CryoDetCmbHcdBpEth/ruckus.tcl
+++ firmware/targets/CryoDetCmbHcdBpEth/ruckus.tcl
@@ -14,3 +14,5 @@ loadConstraints -dir  "$::DIR_PATH/hdl/"
 
 # Place and Route strategy 
 set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+set_property STEPS.POWER_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+set_property STEPS.POST_PLACE_POWER_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
