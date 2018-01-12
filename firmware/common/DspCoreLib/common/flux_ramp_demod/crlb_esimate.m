719158adaf7a30b639ef368943b9d0b54d7590f1
Mitch D'Ewart <mdewart@slac.stanford.edu>
2018-01-12 09:51:25 -0800

    - Add plots for sinewave demod - Update filter so we can look at non-gaussian (filtered) noise for pure sine case

--- firmware/common/DspCoreLib/common/flux_ramp_demod/crlb_esimate.m
+++ firmware/common/DspCoreLib/common/flux_ramp_demod/crlb_esimate.m
@@ -1,19 +1,21 @@
 % Look at demodulation performance vs measurement (frequency) noise
 
-Fs = 2.4e6;              % processing rate
-Fc = 30e3;               % flux ramp carrier rate
-Fr = 10e3;               % flux ramp reset rate
-frameSize = Fs/Fr;  
+Fs           = 2.4e6;    % processing rate
+Fc           = 30e3;     % flux ramp carrier rate
+Fr           = 10e3;     % flux ramp reset rate
+frameSize    = Fs/Fr;  
 numberFrames = 10000;
+filterNoise  = 1;        % filter noise spectrum?.
+a            = 0.01;     % noise filter rolloff
 
 i = 0:(frameSize-1);
 i = i(:);
 
 
-A = 0.25;
+A = 0.5;
 theta = pi/3;
 
-noiseSigma = 0.1;
+noiseSigma = 0.05;
 
 % generate signal for 1 frame
 
@@ -22,7 +24,12 @@ s = A.*cos(2*pi*i*Fc/Fs + theta);
 % extend to numberFrames, add noise
 y = repmat(s, numberFrames, 1);
 
+
 noise = noiseSigma.*randn(size(y));
+if filterNoise == 1
+    noise = filter(a, [-1 1-a], noise);     % filter noise
+    noise = (noiseSigma./std(noise))*noise; % normalize to have same integrated power
+end
 y = y + noise;
 
 % measurement model
@@ -55,10 +62,34 @@ disp(['    achieved: ', num2str(rms), ' radians RMS'])
 disp([' ' ])
 disp([' ' ])
 
+
+figure;
+plot(s)
+hold on
+plot(y(1:length(s),:))
+xlabel('Sample number')
+ylabel('Frequency (Hz)')
+legend('Noiseless response','Noise corrupted')
+title('Pure sinewave demodulation')
+
+figure;
+[pxx, f] = pwelch(noise, [], [], [], 2.4e6);
+semilogx(f,10*log10(pxx))
+ylabel('Power/frequency (Hz/Hz)')
+title('Measurement noise spectrum')
+xlabel('Frequency (Hz)')
+
+figure
+[pxx, f] = pwelch(theta-mean(theta), [], [], [], 2.4e6);
+semilogx(f,10*log10(pxx))
+ylabel('Power/frequency (rad/Hz)')
+title('Demodulated noise spectrum')
+xlabel('Frequency (Hz)')
+
 pause()
 
 %% lets try with SQUID like signal non white noise
-noiseSigma = 200;  % Hz RMS
+noiseSigma = 2000;  % Hz RMS
 modDepth = 150e3; % Hz
 lambda    = 0.3;
 
@@ -80,9 +111,11 @@ s = s*modDepth./0.65;
 % extend to numberFrames, add noise
 y = repmat(s, numberFrames, 1);
 
-alpha = 0.9;
 noise = noiseSigma.*randn(size(y));
-noise = filter(alpha, [1 -alpha], noise);
+if filterNoise == 1
+    noise = filter(a, [-1 1-a], noise);      % filter noise
+    noise = (noiseSigma./std(noise))*noise;  % normalize to have same integrated power
+end
 y = y + noise;
 
 % measurement model
@@ -92,11 +125,13 @@ H = [cos(2*pi*i*Fc./Fs), sin(2*pi*i*Fc./Fs),...
     ones(frameSize,1)];
 
 
+
+
 % frame processing
 theta = zeros(numberFrames, 1);
-for i = 0:numberFrames-1
-    alpha = H\y( (i*frameSize+1):((i+1)*frameSize) );
-    theta(i+1) = atan2(alpha(2), alpha(1));
+for k = 0:numberFrames-1
+    alpha = H\y( (k*frameSize+1):((k+1)*frameSize) );
+    theta(k+1) = atan2(alpha(2), alpha(1));
 end
 
 snr = (A.^2)./(2*noiseSigma.^2);
@@ -119,11 +154,11 @@ disp([' ' ])
 figure;
 plot(s)
 hold on
-plot(y)
+plot(y(1:length(s),:))
 xlabel('Sample number')
 ylabel('Frequency (Hz)')
 legend('Noiseless response','Noise corrupted')
-xlim([1 frameSize*2])
+
 
 figure;
 [pxx, f] = pwelch(noise, [], [], [], 2.4e6);
