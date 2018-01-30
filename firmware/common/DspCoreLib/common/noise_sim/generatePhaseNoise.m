8aa22b3a27324ecd906029aa22e0610f722aa6b1
Mitch D'Ewart <mdewart@slac.stanford.edu>
2018-01-30 11:47:46 -0800

    Rework   - optionally pass Fs for noise generation   - optionally pass filter length for noise shaping   - allow lower start frequency and variable frequency step   - can now generate low frequency noise at low sampling rate

--- firmware/common/DspCoreLib/common/noise_sim/generatePhaseNoise.m
+++ firmware/common/DspCoreLib/common/noise_sim/generatePhaseNoise.m
@@ -27,71 +27,64 @@
 
  
 
-function [phaseNoise, rmsPhaseNoise, freqOut, PxxOut, h] = generatePhaseNoise(freq, dBc, nSamples, Fs)
+function [phaseNoise, rmsPhaseNoise, freqOut, PxxOut, h] = generatePhaseNoise(freq, dBc, nSamples, Fs, varargin)
+
+    numvarargs            = length(varargin);
+    optargs               = {freq(end) + 1, 2000};     % optionally pass freqEnd, filter length
+    optargs(1:numvarargs) = varargin;
 
     % generate phase noise from freqStart to freqEnd
-    freqStart = 0;
-    freqStep  = 1e3;
-    freqEnd   = 2e6;
+    freqStart                 = 1e-2;
+    [freqEnd, filtLength]     = optargs{:};
 
     % produce samples at noiseFs = 2*freqEnd (Nyquist)
     noiseFs     = 2*freqEnd;
     sampleRatio = Fs/noiseFs;
     
-    filtLength = 2000;
-    
     % number of low rate samples to generate
     nGenSamples   = ceil(nSamples/sampleRatio) + filtLength;
     wNoise        = randn(1, nGenSamples);
     
+    % calculate noise power, use to normalizes noise after filtering
+    ssbPower = 0;
+    % integrate power in log log
+    for i = 1:length(dBc)-1
+        p        = 0.5*(dBc(i+1) + dBc(i)) + 10*log10(freq(i+1) - freq(i));
+        ssbPower = ssbPower + 10.^(p./10)
+    end
+    rmsPhaseNoise = sqrt(2*ssbPower)
     
-    freqVec = [freqStart+1e-2     freq  freqEnd];
-    dBcVec  = [dBc(1)                dBc     -200];
-
-    freqVec_dB = 20*log10( freqVec );  % dB relative 1Hz
+    linearFreqVec = [freqStart            freq  freqEnd];
+    logPowerVec   = [dBc(1)                dBc     -200];
+    logFreqVec    = 20*log10( linearFreqVec );
     
-
     % linear interpolate in loglog
-    coarseFreqVec_dB  = ...
-        floor( 20*log10(freqStart+1e-2) ):floor( 20*log10(freqEnd) );
-    coarsePowerVec_dB = ...
-        interp1(freqVec_dB, dBcVec, coarseFreqVec_dB, 'linear');
+    logFreqVecInterp  = ...
+        floor( 20*log10(freqStart) ):floor( 20*log10(freqEnd) );
+    logPowerVecInterp = ...
+        interp1(logFreqVec, logPowerVec, logFreqVecInterp, 'linear');
 
+    linearFreqVecInterp  = 10.^(logFreqVecInterp./20);
+    linearPowerVecInterp = 10.^(logPowerVecInterp./10);
 
-    % resample in linear scale - easier for int egration
-    fineFreqVec     = ...
-        freqStart:freqStep:freqEnd;
-    finePowerVec_dB = ...
-        interp1(10.^(coarseFreqVec_dB./20), coarsePowerVec_dB, ...
-                fineFreqVec, 'linear', 'extrap');
-    finePowerVec    = ...
-        10.^(finePowerVec_dB./10);
-    
+    % build filter in frequency domain
+    % resample freq, power in linear scale
+    power         = zeros(1, filtLength);
+    n             = 0:(filtLength/2-1);
+    freqResample  = n.*noiseFs/filtLength;
+    powerResample = interp1(linearFreqVecInterp, linearPowerVecInterp, ...
+                      freqResample, 'linear','extrap');
 
+    % make symmetric filter (real coefficients)
+    power( n + 1 )              = powerResample;
+    power( filtLength - n )     = power( 2 + n );
+    power( filtLength/2 +1 )    = 0;
 
-
-    % noise power, use to normalizes noise after filtering
-    ssbPower      = freqStep*cumtrapz(finePowerVec);
-    rmsPhaseNoise = sqrt(2*ssbPower(end));
-    
-    fineFreqVec  = 10.^(coarseFreqVec_dB./20);
-    finePowerVec = 10.^(coarsePowerVec_dB./10);
-    
-    freqPos  = (0:filtLength/2-1)*noiseFs/filtLength;
-    freqNeg  = (-filtLength/2:-1)*noiseFs/filtLength;
-    powerPos = interp1(fineFreqVec, finePowerVec, ...
-                      freqPos, 'linear','extrap');
-    powerNeg = interp1(fineFreqVec, finePowerVec, ...
-                      abs(freqNeg), 'linear','extrap');
-    
-    power = [powerPos powerNeg];
     mag   = sqrt(power);
-    
-    
     h = fftshift(ifft(mag));
     window = kaiser(filtLength+2);
-    h = h.*window(2:end-1)';
-    
+    h = real(h).*window(2:end-1)';
+
 %     d = fdesign.arbmag('N,F,A',filtLength,[0 fineFreqVec(2:end)]./fineFreqVec(end),sqrt(finePowerVec));
 %     Hd = design(d,'freqsamp', 'window' ,{@kaiser,20},'SystemObject',true);
 %     h = Hd.Numerator;
@@ -106,8 +99,8 @@ function [phaseNoise, rmsPhaseNoise, freqOut, PxxOut, h] = generatePhaseNoise(fr
     phaseNoise    = phaseNoise(1,1:nSamples);
     phaseNoise    = phaseNoise*( rmsPhaseNoise/std(phaseNoise) );
     
-    freqOut = fineFreqVec;
-    PxxOut  = finePowerVec;
+    freqOut = linearFreqVecInterp;
+    PxxOut  = linearPowerVecInterp;
     
     if nargout < 1
         [Pxx, f] = pwelch(phaseNoise, [], 0, [], Fs);
