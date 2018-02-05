cbf396986678a562a5bfd5464b7b0620e22a0502
Mitch D'Ewart <mdewart@slac.stanford.edu>
2018-02-05 09:07:21 -0800

    Add synthesis options to help timing to App ruckus

--- firmware/common/CryoApp/ruckus.tcl
+++ firmware/common/CryoApp/ruckus.tcl
@@ -8,3 +8,11 @@ if { [info exists ::env(CRYO_DET_TYPE)] != 1 } {
 # Load Source Code
 loadRuckusTcl "$::DIR_PATH/AppCore"
 loadRuckusTcl "$::DIR_PATH/../DspCoreLib/$::env(CRYO_DET_TYPE)"
+
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
+
+set_property STEPS.POWER_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+set_property STEPS.POST_PLACE_POWER_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+
+set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY rebuilt [get_runs synth_1]
+set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING          on      [get_runs synth_1]
