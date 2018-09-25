f1a964e3b60d5519dfe0b2a16bab9401ad770aca
Jesus Vasquez <jesusvasquez333@gmail.com>
2018-09-25 08:46:11 -0700

    Update setup script for smurf server enviroment

--- software/setup.sh
+++ software/setup.sh
@@ -1,12 +1,15 @@
 ###################
 # Setup environment
 ###################
-source /afs/slac/g/reseng/rogue/pre-release/setup_env.sh
-#source /afs/slac/g/reseng/rogue/v2.12.0/setup_env.sh
+#source /afs/slac/g/lcls/package/pyrogue/rogue/v2.12.0/setup_slac.sh
+#source /afs/slac/g/lcls/package/pyrogue/rogue/v2.12.0/setup_rogue.sh
 
 # Python Package directories
-export PCIE_DIR=${PWD}/../firmware/submodules/axi-pcie-core/python
-export SURF_DIR=${PWD}/../firmware/submodules/surf/python
+TOP=/usr/local/controls/Applications/smurf/cmb_Det/smurf-pcie/master/software
+export AMCC_DIR=${TOP}/../firmware/submodules/amc-carrier-core/python
+export PCIE_DIR=${TOP}/../firmware/submodules/axi-pcie-core/python
+export TIMING_DIR=${TOP}/../firmware/submodules/lcls-timing-core/python
+export SURF_DIR=${TOP}/../firmware/submodules/surf/python
 
 # Setup python path
-export PYTHONPATH=${PWD}/python:${PCIE_DIR}:${SURF_DIR}:${PYTHONPATH}
+export PYTHONPATH=${TOP}/python:${AMCC_DIR}:${PCIE_DIR}:${TIMING_DIR}:${SURF_DIR}:${PYTHONPATH}
