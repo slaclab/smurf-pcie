5817c021eb680132b53ab0ab63f2ca6dffe4d38d
Larry Ruckman <ruckman@slac.stanford.edu>
2018-11-16 11:29:10 -0800

    adding AppDataProcessorPktMon to FW/SW

--- software/python/SmurfKcu1500RssiOffload/_AppDataProcessor.py
+++ software/python/SmurfKcu1500RssiOffload/_AppDataProcessor.py
@@ -10,6 +10,51 @@
 ##############################################################################
 
 import pyrogue as pr
+import surf.axi 
+
+class AppDataProcessorPktMon(pr.Device):
+    def __init__(   self,       
+            name        = "AppDataProcessorPktMon",
+            description = "Container for AppDataProcessorPktMon",
+            **kwargs):
+        super().__init__(name=name, description=description, **kwargs)
+        
+        self.addRemoteVariables(   
+            name         = 'Header',
+            description  = 'Current Header values',
+            offset       = 0x0,
+            bitSize      = 64,
+            mode         = 'RO',
+            number       = 16,
+            stride       = 8,
+        )
+        
+        self.add(pr.RemoteVariable(
+            name         = 'SofErrCnt',
+            description  = 'Increments every time there is a SOF error detected',
+            offset       = 0x84,
+            bitSize      = 32,
+            mode         = 'RO',
+        ))
+
+        self.add(pr.RemoteVariable(
+            name         = 'SkipErrCnt',
+            description  = 'Increments every time there is a seqCnt skip error detected',
+            offset       = 0x88,
+            bitSize      = 32,
+            mode         = 'RO',
+        ))
+
+        self.add(pr.RemoteVariable(
+            name         = 'EoffErrCnt',
+            description  = 'Increments every time there is an EOFE error detected',
+            offset       = 0x8C,
+            bitSize      = 32,
+            mode         = 'RO',
+        ))        
+        
+    def countReset(self):
+        self._rawWrite(offset=0x80,data=0x1)          
 
 class AppDataProcessor(pr.Device):
     def __init__(   self,       
@@ -18,4 +63,17 @@ class AppDataProcessor(pr.Device):
             **kwargs):
         super().__init__(name=name, description=description, **kwargs)
         
-        # Placeholder for future code
+        # AXI Stream Inbound/Outbound Monitor        
+        self.add(surf.axi.AxiStreamMonitoring(            
+            name        = 'AxisMon', 
+            offset      = 0x00000, 
+            numberLanes = 2,
+            expand      = False,
+        ))    
+
+        # Inbound Packet Monitor        
+        self.add(AppDataProcessorPktMon(            
+            name        = 'PktMon', 
+            offset      = 0x10000, 
+            expand      = False,
+        ))            
