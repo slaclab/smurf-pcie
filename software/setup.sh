feca8a6c1299050096f3deb8d3a3dde5215f5e51
Larry Ruckman <ruckman@slac.stanford.edu>
2025-04-01 14:37:38 -0700

    send ACK if local BUSY event detected

--- software/setup.sh
+++
@@ -1,2 +0,0 @@
-source /afs/slac.stanford.edu/g/reseng/vol31/anaconda/anaconda3/etc/profile.d/conda.sh
-conda activate rogue_v6.4.0
---
+++ software/setup_env_slac.sh
@@ -0,0 +1,9 @@
+##################################
+# Setup environment
+##################################
+source /sdf/group/faders/users/$USER/miniforge3/etc/profile.d/conda.sh
+
+##################################
+# Activate Rogue conda Environment
+##################################
+conda activate rogue_v6.5.0
