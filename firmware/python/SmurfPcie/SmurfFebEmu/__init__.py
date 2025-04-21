f7a7d1922fa8a41569e415149a2b76fb0e999a91
Larry Ruckman <ruckman@slac.stanford.edu>
2025-04-21 09:30:52 -0700

    updating code headers

--- firmware/python/SmurfPcie/SmurfFebEmu/__init__.py
+++ firmware/python/SmurfPcie/SmurfFebEmu/__init__.py
@@ -1,2 +1,11 @@
+##############################################################################
+# This file is part of the 'smurf-pcie'.
+# It is subject to the license terms in the LICENSE.txt file found in the
+# top-level directory of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'smurf-pcie', including this file,
+# may be copied, modified, propagated, or distributed except according to
+# the terms contained in the LICENSE.txt file.
+##############################################################################
 from SmurfPcie.SmurfFebEmu._Feb import *
 from SmurfPcie.SmurfFebEmu._Root import *
